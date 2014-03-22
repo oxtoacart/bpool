a59c0daaf5432f49f09c0aef1ab472b0652ad0c8
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-22 12:05:25 -0500

    Performance improvements

--- bytepool.go
+++ bytepool.go
@@ -10,13 +10,13 @@ type BytePool struct {
 }
 
 /*
-NewBytePool creates a new BytePool bounded to the given size, with new byte
-arrays sized based on maxWidth.
+NewBytePool creates a new BytePool bounded to the given maxSize, with new byte
+arrays sized based on width.
 */
-func NewBytePool(size int, maxWidth int) (bp *BytePool) {
+func NewBytePool(maxSize int, width int) (bp *BytePool) {
 	return &BytePool{
-		c: make(chan []byte, size),
-		w: maxWidth,
+		c: make(chan []byte, maxSize),
+		w: width,
 	}
 }
 
@@ -39,5 +39,17 @@ func (bp *BytePool) Get() (b []byte) {
 Put returns the given Buffer to the BytePool.
 */
 func (bp *BytePool) Put(b []byte) {
-	bp.c <- b
+	select {
+	case bp.c <- b:
+		// buffer went back into pool
+	default:
+		// buffer didn't go back into pool, just discard
+	}
+}
+
+/*
+Width returns the width of the byte arrays in this pool.
+*/
+func (bp *BytePool) Width() (n int) {
+	return bp.w
 }
